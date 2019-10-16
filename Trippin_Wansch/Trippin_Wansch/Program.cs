668eabc952c5aff821432508573e63c0ead89948
Stefan Wansch <stefan-wansch@gmx.at>
2019-10-16 10:25:16 +0200

    Finished Homework

---
+++ Trippin_Wansch/Trippin_Wansch/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Trippin_Wansch
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            User_Request c = new User_Request();
+            c.readJson();
+            await c.compareUser();
+        }
+    }
+}
---
+++ Trippin_Wansch/Trippin_Wansch/User_Request.cs
@@ -0,0 +1,71 @@
+﻿
+
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Trippin_Wansch
+{
+    class User_Request
+    {
+        List<User> allUser;
+        private static readonly HttpClient client = new HttpClient() { BaseAddress = new Uri("https://services.odata.org/TripPinRESTierService/(S(gfyapivcxr5wmab2lxogm3ac))/") };
+
+        public User_Request()
+        {        }
+
+        public void readJson()
+        {
+            IConfiguration config = new ConfigurationBuilder().AddJsonFile("config.json").Build();
+            var jsonInput = config["jsonFile"];
+            StreamReader r = new StreamReader(jsonInput);
+            var json = r.ReadToEnd();
+            allUser = JsonConvert.DeserializeObject<List<User>>(json);
+            
+        }
+
+        public async Task compareUser()
+        {
+           
+            foreach(var item in allUser)
+            {
+                HttpResponseMessage response = await client.GetAsync($"People('{item.UserName}')");
+                Console.WriteLine(response.StatusCode.ToString());
+                if (!response.IsSuccessStatusCode)
+                {
+                    HttpResponseMessage postResponse = await client.PostAsync("People",
+                           new StringContent(System.Text.Json.JsonSerializer.Serialize(new
+                           {
+                               item.UserName,
+                               item.FirstName,
+                               item.LastName,
+                               Emails = new List<string> { item.Email },
+                               AddressInfo = new List<object>{
+                                    new
+                                    {
+                                        item.Address,
+                                        City = new
+                                        {
+                                            Name = item.CityName,
+                                            CountryRegion = item.Country,
+                                            Region = item.Country
+                                        }
+                                    }
+                               }
+                           }), Encoding.UTF8, "application/json"));
+                    Console.WriteLine("New added User: "+item.UserName);
+                }
+                else
+                {
+                    Console.WriteLine("User already exists");
+                }
+            }
+        }
+    }
+}
